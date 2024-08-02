9a060dfc5999a5d16d13da81e0c3a4782df1d80e
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-02 10:32:29 +0530

    Create arithmetic.py

---
+++ OPERATORS/arithmetic.py
@@ -0,0 +1,18 @@
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+
+sum = num1 + num2
+dif = num1 - num2
+pro = num1 * num2
+div = num1 / num2
+flodiv = num1 // num2
+rem = num1 % num2
+exp = num1 ** num2
+
+print("sum is : ", sum)
+print("Difference is: ", dif)
+print("product is : ", pro)
+print("Division is: ", div)
+print("FloorDivision is : ", flodiv)
+print("Reminder is: ", rem)
+print("the exponential is: ", exp)
