b4d5b418219a72c60d1eafc2fb693f4fded3f238
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-02 10:47:28 +0530

    Add files via upload

---
+++ OPERATORS/comparison.py
@@ -0,0 +1,7 @@
+x = 20
+print("using > operator, result:", x >23)
+print("using < operator, result:", x <23)
+print("using >= operator, result:", x >= 23)
+print("using <= operator, result:", x <= 23)
+print("using == operator, result:", x == 23)
+print("using != operator, result:", x !=23)
