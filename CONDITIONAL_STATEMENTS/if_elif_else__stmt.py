ad352623f8be649fca804aa2ec9bedca3c5211dc
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-02 21:46:56 +0530

    Create if_elif_else__stmt.py

---
+++ CONDITIONAL_STATEMENTS/if_elif_else__stmt.py
@@ -0,0 +1,7 @@
+num = int(input("enter a number:"))
+if num < 0:
+    print("The number entered is negative")
+elif num> 0:
+    print("the number entered is positive")
+else:
+    print("the number entered is Zero")
