d5bae75cd4a004b40fbf84352b4cf9935ad4fc4d
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-02 21:51:15 +0530

    Create circle.py

---
+++ CONDITIONAL_STATEMENTS/circle.py
@@ -0,0 +1,5 @@
+num = int(input("enter the radius of a circle:"))
+area = 3.14 * num**2
+perimeter = 2 * 3.14 * num
+print("area of the circle is:", area)
+print("perimeter of the circle is:", perimeter)
