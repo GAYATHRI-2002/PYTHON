c7eaf3196cd14c6b1f333945eec1584fad0d33f6
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-02 10:52:40 +0530

    Add files via upload

---
+++ OPERATORS/assignment.py
@@ -0,0 +1,23 @@
+x = 20
+print("value of x :", x)
+
+x +=5
+print("value of x :", x)
+
+x -= 10
+print("value of x :", x)
+
+x /= 3
+print("value of x :", x)
+
+x //= 3
+print("value of x :", x)
+
+x *= 10
+print("value of x :", x)
+ 
+x **= 2
+print("value of x :", x)
+
+x %= 2
+print("value of x :", x)
