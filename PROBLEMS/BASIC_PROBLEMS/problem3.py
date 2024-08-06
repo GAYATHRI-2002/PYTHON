a5826d7e5297b177db8832a1ddd95be71692c1c0
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-06 22:06:38 +0530

    Create problem3.py

---
+++ PROBLEMS/BASIC_PROBLEMS/problem3.py
@@ -0,0 +1,5 @@
+#Python program to find the area of a triangle
+base= int(input("enter a base : "))
+height = int(input("enter the height : "))
+area = (1/2)* base * height
+print("the area of triangle: ",area)
