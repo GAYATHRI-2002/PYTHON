a0f4477273a8be54900d5abeb527af0dc616d9a6
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-07 10:51:59 +0530

    Create problem5.py

---
+++ PROBLEMS/BASIC_PROBLEMS/problem5.py
@@ -0,0 +1,23 @@
+#program to swap two variables
+num1 = 5
+num2 = 8
+print("numbers before swapping: ",num1,num2)
+temp = 0
+temp = num1
+num1 = num2
+num2 = temp
+print("after swapping: ",num1,num2)
+
+#by using arithmrtic operations
+
+p = 10
+q = 5
+print("num1 :",p)
+print("num2 : ",q)
+print("Aftter swapping")
+p = p +q
+q = p - q
+p = p - q
+print("after swapping results: ",p,q)
+
+
