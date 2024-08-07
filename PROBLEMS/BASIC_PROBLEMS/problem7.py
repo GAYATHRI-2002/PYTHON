5271c3533cbd0d61f868ca892d4bd11e7cf670f9
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-08-07 22:07:24 +0530

    Create problem7.py

---
+++ PROBLEMS/BASIC_PROBLEMS/problem7.py
@@ -0,0 +1,6 @@
+#Python Program to check if a Number is Positive, Negative or Zero
+num1 = int(input("enter a number: "))
+if num1 > 0:
+    print("the number is positive")
+else:
+    print("it is negative")
