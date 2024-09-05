2c0223786cd60353e3ad2f266bcd5dd515c6ff4a
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-09-05 21:57:05 +0530

    Create madlib.py

---
+++ MINI-PROJECTS/madlib.py
@@ -0,0 +1,7 @@
+# MADLIB GAME
+adj = input("enter an adjective: ")
+verb = input("enter a verb: ")
+time = input("enter a time: ")
+noun = input("enter a noun: ")
+print(f"i love programing because it is so {adj}.when i code i feel like {verb}.i use to code at {time}.after i clear a bug "
+      f"i become {noun}")
